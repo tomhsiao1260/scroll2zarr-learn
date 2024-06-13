893589e305026b651617278aec81da06587e5e66
Hsiao Yao <tomtogo1260@gmail.com>
2024-06-13 19:58:08 +0800

    use argparse

--- scroll_to_ome.py
+++ scroll_to_ome.py
@@ -1,7 +1,20 @@
 import sys
+import argparse
 
 def main():
-    print('hello')
+    parser = argparse.ArgumentParser(
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            description="Create OME/Zarr data store from a set of TIFF files")
+    parser.add_argument(
+            "input_tiff_dir", 
+            help="Directory containing tiff files")
+    parser.add_argument(
+            "output_zarr_ome_dir", 
+            help="Name of directory that will contain OME/zarr datastore")
+
+    args = parser.parse_args()
+
+    print(args.input_tiff_dir, args.output_zarr_ome_dir)
 
 if __name__ == '__main__':
     sys.exit(main())
