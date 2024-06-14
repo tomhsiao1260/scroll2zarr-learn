baac9868c98637766b921c09bc00eb9b8912105f
Hsiao Yao <tomtogo1260@gmail.com>
2024-06-14 12:42:07 +0800

    tifs2zarr init

--- scroll_to_ome.py
+++ scroll_to_ome.py
@@ -1,6 +1,10 @@
 import sys
+import shutil
 import argparse
 from pathlib import Path
+
+def tifs2zarr(tiffdir, zarrdir, chunk_size):
+    print('tifs2zarr init')
 
 def main():
     parser = argparse.ArgumentParser(
@@ -12,6 +16,15 @@ def main():
     parser.add_argument(
             "output_zarr_ome_dir", 
             help="Name of directory that will contain OME/zarr datastore")
+    parser.add_argument(
+            "--chunk_size", 
+            type=int, 
+            default=128, 
+            help="Size of chunk")
+    parser.add_argument(
+            "--zarr_only", 
+            action="store_true", 
+            help="Create a simple Zarr data store instead of an OME/Zarr hierarchy")
 
     args = parser.parse_args()
 
@@ -24,6 +37,25 @@ def main():
     if not tiffdir.exists():
         print("Input TIFF directory",tiffdir,"does not exist")
         return 1
+
+    chunk_size = args.chunk_size
+    zarr_only = args.zarr_only
+    
+    slices = None
+    zarr_only = True
+    
+    if zarr_only:
+        if zarrdir.exists():
+            print("removing", zarrdir)
+            shutil.rmtree(zarrdir)
+
+    if zarr_only:
+        err = tifs2zarr(tiffdir, zarrdir, chunk_size)
+        if err is not None:
+            print("error returned:", err)
+            return 1
+        return
+
 
 if __name__ == '__main__':
     sys.exit(main())
